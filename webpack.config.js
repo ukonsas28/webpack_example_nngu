251c459ece1a1382a185d83085361e3dc1897560
Alexey Shirokov <shirokovam@lad24.ru>
2022-12-06 17:31:15 +0300

    webpack config init

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+};
